a84cc48aef22a26438748d4eb34de03f1bffaa7a
MidhatJalil <MIDHATJALIL345@GMAIL.COM>
2024-10-21 19:32:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function generateEven(){
+    let inputValue = document.getElementById('inputValue').value;
+    let i = 0;
+    let value = '';
+    while (i <= inputValue) {
+        if(i % 2 === 0){
+            value += `${i} `;
+        }
+        i++;
+    }
+    document.getElementById('result').innerHTML = value;
+}
+
+function reset(){
+    let inputValue = document.getElementById('inputValue').value='';
+    document.getElementById('result').innerHTML = '';
+
+
+
+}
